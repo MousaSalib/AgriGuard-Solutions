c07bf30b984a457ed0f4eee52b43a8d8ed205fac
Mousa Khairy Halim Salib <moussa.kheiry@gmail.com>
2024-11-24 12:19:50 +0200

    I styled the not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,16 +2,19 @@ import Link from "next/link";
 
 const NotFoundPage = () => {
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section>
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="mx-auto max-w-screen-sm text-center">
-          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-customPink dark:text-primary-500">
+          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-customPink dark:text-primary-500 text-red-500">
             404
           </h1>
-          <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+          <p
+            className="mb-4 text-3xl tracking-tight font-bold md:text-4xl"
+            style={{ color: "#ffff00" }}
+          >
             Something&#39;s missing.
           </p>
-          <p className="mb-4 text-2xl font-light text-gray-500 dark:text-gray-400">
+          <p className="mb-4 text-2xl font-semibold">
             Sorry, we can&#39;t find that page. You&#39;ll find lots to explore
             on the home page.
           </p>
